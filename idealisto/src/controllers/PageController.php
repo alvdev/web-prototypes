651c882a01963c31804b9202509f3bb6af52814c
alv.dev <soy@alv.dev>
2021-09-21 09:11:10 +0200

    Added global page controller

--- idealisto/src/controllers/PageController.php
+++ idealisto/src/controllers/PageController.php
@@ -4,5 +4,21 @@ namespace app\controllers;
 
 class PageController
 {
-    
+    /* TODO: runBeforeAction method for contact page */
+    public function runAction($actionName)
+    {
+        if (method_exists($this, 'runBeforeAction')) {
+            $result = $this->runBeforeAction();
+            if ($result == false) {
+                return;
+            }
+        }
+
+        $actionName .= 'Action';
+        if (method_exists($this, $actionName)) {
+            $this->$actionName();
+        } else {
+            include VIEW . '404.php';
+        }
+    }
 }
