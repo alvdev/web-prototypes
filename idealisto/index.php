df4581c4e4427f2e57df112a00b2c293dccc1b5f
alv.dev <soy@alv.dev>
2021-09-21 09:10:35 +0200

    Added page controllers

--- idealisto/index.php
+++ idealisto/index.php
@@ -1,14 +1,21 @@
 <?php
 
+use app\controllers\HomePageController;
+use app\controllers\LoginPageController;
+use app\controllers\Template;
+
 require_once 'vendor/autoload.php';
 require_once 'config/config.php';
-include_once 'src/helpers/locale.php';
 
 // Getting page url
-$page = $_GET['page'] ?? 'home';
+$page = $_GET['page'] ?? $_POST['page'] ?? 'home';
+$action = $_GET['action'] ?? $_POST['action'] ?? 'default';
 
 if ($page === 'login') {
-    include_once CTRL . 'loginPage.php';
+    $loginPageController = new LoginPageController();
+    $loginPageController->runAction($action);
 } else {
-    include_once CTRL . 'homePage.php';
+    include_once CTRL . 'HomePage.php';
+    $homePageController = new HomePageController();
+    $homePageController->runAction($action);
 }
