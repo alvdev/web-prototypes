c72cd55dcd3047a4ba93c8070b10bd25c71ce3a0
alv.dev <soy@alv.dev>
2021-09-23 09:12:07 +0200

    Added login form

--- idealisto/src/views/login.php
+++ idealisto/src/views/login.php
@@ -2,4 +2,22 @@
 
 <h1><?= $lang['LOGIN_TITLE'] ?></h1>
 
+<?= $variables['title'] ?>
+
+<form id="login" action="" method="post">
+    <div class="wrapper">
+        <div>
+            <label for="username"><?= $lang['USERNAME'] ?></label>
+            <input id="username" type="text" name="username">
+        </div>
+
+        <div>
+            <label for="password"><?= $lang['PASSWORD'] ?></label>
+            <input id="password" type="password" name="password">
+        </div>
+    </div>
+
+    <button class="btn submit"><?= $lang['LOGIN'] ?></button>
+</form>
+
 <?php include_once 'templates/footer.php' ?>
