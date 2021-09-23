1af54dd5c8567ce2752933e12162bd16d5f91b25
alv.dev <soy@alv.dev>
2021-09-23 09:15:02 +0200

    Fetch login page

--- idealisto/public/js/script.js
+++ idealisto/public/js/script.js
@@ -9,3 +9,18 @@ function setPrice() {
   // Add formatted price value to the HTML <output> placeholder
   priceOutput.textContent = Number(price.value).toLocaleString() + '€';
 }
+
+// Fetch login page and show it at the index page
+fetch('?page=login')
+  .then(res => res.text())
+  .then(data =>
+    loginForm = data.substring(data.indexOf('<main>') + 8, data.indexOf('</main>'))
+  )
+
+loginbtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const loginPlace = document.querySelector('#login-form');
+  loginPlace.innerHTML = loginForm;
+  loginPlace.classList.toggle('show')
+  loginbtn.classList.toggle('close');
+})
