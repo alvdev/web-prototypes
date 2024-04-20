29032b74f6eea50d2b83c4a083b6aaba0ed86f15
alv.dev <soy@alv.dev>
2024-04-20 23:06:25 +0200

    feat: add addr flag to cli

--- pastext/cmd/web/main.go
+++ pastext/cmd/web/main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":8080", "Listen HTTP address")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./ui/dist/"))
+
 	mux.Handle("/dist/", http.StripPrefix("/dist/", fileServer))
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/text/view", textView)
 	mux.HandleFunc("/text/create", textCreate)
 
-	log.Print("Starting server on :8080")
-	err := http.ListenAndServe(":8080", mux)
+	log.Printf("Starting server on %v", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
