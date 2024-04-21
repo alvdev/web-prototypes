aa3756fab060b4f5c43a2e8292183ae3a22eb5d8
alv.dev <soy@alv.dev>
2024-04-21 02:41:48 +0200

    add custom log messages

--- pastext/cmd/web/main.go
+++ pastext/cmd/web/main.go
@@ -4,9 +4,13 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	logInfo := log.New(os.Stdout, "INFO:\t", log.Ldate|log.Ltime)
+	logErr := log.New(os.Stderr, "ERROR:\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	addr := flag.String("addr", ":8080", "Listen HTTP address")
 	flag.Parse()
 
@@ -18,7 +22,7 @@ func main() {
 	mux.HandleFunc("/text/view", textView)
 	mux.HandleFunc("/text/create", textCreate)
 
-	log.Printf("Starting server on %v", *addr)
+	logInfo.Printf("Starting server on %v", *addr)
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	logErr.Fatal(err)
 }
